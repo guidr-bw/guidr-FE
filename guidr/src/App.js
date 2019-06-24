e2e2079a7e51cead06cd9e2d25e14b3f4f1c6e53
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-06-24 16:32:08 -0600

    WIP built out new trip component

--- guidr/src/App.js
+++ guidr/src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom'
 import './App.css';
 import LoginForm from './components/LoginForm';
 import CreateForm from './components/CreateForm';
+import NewTrip from './components/NewTrip';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <h1>guidr</h1>
       <Route exact path='/' component={LoginForm} />
       <Route path='/create-account' component={CreateForm} />
+      <Route path='/new-trip' component={NewTrip} />
     </div>
   );
 }
---
+++ guidr/src/components/NewTrip.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+class NewTrip extends React.Component {
+    state = {
+        tripName: '',
+        timeFrame: '',
+        length: '',
+        tripType: '',
+        description: '',
+    }
+
+    changeHandler = e => {
+      e.preventDefault();
+      this.setState({
+        [e.target.name]: e.target.value
+      })
+    }
+
+    render(){
+        return(
+            <div>
+                <h4>Name of Trip</h4>
+                <input 
+                  type='text'
+                  name='tripName'
+                  value={this.state.tripName}
+                  onChange={this.changeHandler}
+                />
+                <h4>Timeframe</h4>
+                <input 
+                  type='number'
+                  name='timeFrame'
+                  value={this.state.timeFrame}
+                  onChange={this.changeHandler}
+                />
+                <form>
+                    <label><input type='radio' name='business' value='professional'/>Professional</label>
+                    <label><input type='radio' name='business' value='personal'/>Personal</label>
+                </form>
+                <h4>Length</h4>
+                <input 
+                  type='number'
+                  name='length'
+                  value={this.state.length}
+                  onChange={this.changeHandler}
+                />
+                <form>
+                    <label><input type='radio' name='duration' value='miles' />Miles</label>
+                    <label><input type='radio' name='duration' value='km' />Km</label>
+                    <label><input type='radio' name='duration' value='days' />Days</label>
+                </form>
+                <h4>Type of Trip</h4>
+                <input 
+                  type='text'
+                  name='tripType'
+                  value={this.state.tripType}
+                  onChange={this.changeHandler}
+                />
+                <h4>Description</h4>
+                <input 
+                  type='text'
+                  name='description'
+                  value={this.state.description}
+                  onChange={this.changeHandler}
+                /><br />
+                <button>Submit</button>
+            </div>
+        )
+    }
+}
+
+export default NewTrip
